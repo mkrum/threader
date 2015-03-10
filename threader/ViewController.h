2502bf55135bcb50179d470c46a64bcadb4d0446
Michael Krumdick <mrkrumdick@gmail.com>
2015-03-10 16:53:51 -0500

    Initial Commit

---
+++ threader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  threader
+//
+//  Created by Michael Krumdick on 3/10/15.
+//  Copyright (c) 2015 mkrum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ threader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  threader
+//
+//  Created by Michael Krumdick on 3/10/15.
+//  Copyright (c) 2015 mkrum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
